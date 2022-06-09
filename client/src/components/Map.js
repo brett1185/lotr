4e1308514bc24973b844c985e17e14b985710339
brett1185 <89299506+brett1185@users.noreply.github.com>
2022-06-09 10:20:07 -0400

    commit

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import '../App.css';
 const Map=(props)=>{
+
+    const navigate=useNavigate()
 
 
 
 
     return(
         <div className='map'>
-                <a className='shireLink' href='https://en.wikipedia.org/wiki/The_Shire'
-                
-                
-                >The Shire</a>
+                <div>
+            
+                <a href='https://en.wikipedia.org/wiki/The_Shire'
+                className='shireLink'>The Shire</a>
+                </div>
         </div>
     )
 }
