01cc4e9c43ead6e8c556e2031d6a3e53b133c401
brett1185 <89299506+brett1185@users.noreply.github.com>
2022-07-07 14:20:47 -0400

    commit

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -6,11 +6,9 @@ const Map=(props)=>{
     return(
         <div className='map'>
                 <div className='shireLink'>
-            
-                <a href='https://en.wikipedia.org/wiki/The_Shire'>
-                    <img src='../public/shire.jpeg' alt='The Shire'/>
-                </a>
+                <a href='https://en.wikipedia.org/wiki/The_Shire'>The Shire</a>
                 </div>
+                <div className='mordor'><a href='https://en.wikipedia.org/wiki/Mordor'>Mordor</a></div>
         </div>
     )
 }
