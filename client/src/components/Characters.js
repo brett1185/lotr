c3a5d4ae3cea1070a76eec34d7accad6940d036c
brett1185 <89299506+brett1185@users.noreply.github.com>
2022-07-18 13:19:04 -0400

    commit

--- client/src/components/Characters.js
+++ client/src/components/Characters.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Results from '../views/Results'
+import ViewAlphabetically from '../views/viewAlphabetically'
 
 
 const Characters=(props)=>{
@@ -11,12 +12,17 @@ const Characters=(props)=>{
     const regJtoL = new RegExp(/^[J-L]/g)
     const regMtoO = new RegExp(/^[M-O]/g)
     const regPtoR = new RegExp(/^[P-R]/g)
-    const regStoU = new RegExp(/^[S-U,s-u]/g)
+    const regStoU = new RegExp(/^[S-U]/g)
     const regVtoZ = new RegExp(/^[V-Z]/g)
 
     
     const [search, setSearch]=useState('')
     const [characterList, setCharacterList] = useState ([])
+
+    const searchTool=(e)=>{
+        e.preventDefault()
+        setSearch(e.target.value)
+    }
 
     useEffect(()=>{
         const headers={
@@ -84,7 +90,20 @@ const Characters=(props)=>{
             <div key={index}>
                 <Link className='text' to={`/view/characters/${names._id}`}style={{fontSize:'50px', margin:'5px'}}>{names.name}</Link>
                 </div>
-        ))}
+        ))},
+        {label: 'Search by name', content: <div style={{display: 'flex', justifyContent:'center', flexDirection:'column'}}>
+            <input style={{width: '100px', marginLeft:'46%'}} type='text' placeholder='type name here...' onChange={(e)=>setSearch(e.target.value)}/>
+            {characterList.filter((name)=>{
+                if (search === ''){
+                    return name}
+                else if (
+                    name.name.toLowerCase().includes(search)){
+                    return name}})
+                .map((names, index)=>(
+            <Link key ={index} className='text' to={`/view/characters/${names._id}`} style={{fontSize:'50px', margin:'5px'}}>{names.name}</Link>
+            ))}
+        </div>
+    }
     ]
     const [ allTabs, setAllTabs ] = useState(alphArray);
 
@@ -103,20 +122,21 @@ const Characters=(props)=>{
 
 
         return(
-        <div >
-            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-evenly', marginTop:'30px'}}>
-                {
-                    allTabs.map((item, index) => (
+            <div>
+                <div style={{display:'flex', flexDirection:'row', justifyContent:'space-evenly', marginTop:'30px'}}>
+                    {allTabs.map((item, index) => (
             <div key={index} className={`tab ${ tabStyle(index) }`} onClick={()=>setActiveTab(index)}>
                 { item.label }
             </div>
         ))
-        }
-            </div>
-    
+                    }
+            </div>    
+
+         {/* <ViewAlphabetically allTabs={allTabs} currentTabIndex={currentTabIndex} setCurrentTabIndex={setCurrentTabIndex} /> */}            
+
     
             <Results tabText={alphArray[activeTab].content}/>
-        </div>
+            </div>
     )
 }
 
