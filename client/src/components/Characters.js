f113e844c31e5788d42956e6257631a9b8fc40fd
brett1185 <89299506+brett1185@users.noreply.github.com>
2022-07-24 15:11:44 -0400

    commit

--- client/src/components/Characters.js
+++ client/src/components/Characters.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Results from '../views/Results'
 import ViewAlphabetically from '../views/viewAlphabetically'
-import Pagination from './Pagination'
+import Pagination from '../views/Pagination'
 
 
 const Characters=(props)=>{
--- client/src/views/Pagination.js
+++ client/src/views/Pagination.js
@@ -1,18 +1,21 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import { matchPath } from 'react-router'
 
 const Pagination=(props)=>{
-    const {characterList}=props
+    const {characterList} = props
     const [currentPage, setCurrentPage]=useState(1)
     const [listPages, setListPages]=useState(0)
-    const [perPage, setPerPage]=useState(10)
     const [maxPageNumberLimit, setMaxPageNumberLimit]=useState(5)
     const [minPageNumLimit, setMinPageNumLimit]=useState(0)
     const [pages, setPages]=useState([])
+    const perPage=10
 
+for( let i =1; i <Math.ceil(characterList.length/perPage); i++){
+    pages.push(i)
+    return pages
+}
 
-    for(let i=1; i<Math.ceil(characterList/perPage); i++){
-        pages.push(i)
-    }
+console.log(pages)
 
     const showPage=(e)=>{
         setCurrentPage(Number(e.target.id))
@@ -21,19 +24,20 @@ const Pagination=(props)=>{
     return(
         <div style={{display:'flex', flexDirection:'row', justifyContent:'space-evenly', border:'2px solid red'}}>
             <ul>
-            {pages.map((number, index)=>{
+            {pages.map((number)=>{
                 if(number < maxPageNumberLimit +1 && number > minPageNumLimit){
                     <li 
-                    key={index}
+                    key={number}
                     id={number}
                     onClick={showPage}
-                    className={currentPage === number? 'active':null}>
+                    className={currentPage === number? 'active':null}
+                    className='text'>
                         {number}
                     </li>
                 }
                 else{
                     return null}
-    })}
+                })}
     </ul>
         </div>
     )
